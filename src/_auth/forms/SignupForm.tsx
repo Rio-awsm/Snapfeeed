afdfd9f3bda5a1f29e2c354a67ef0ee7db9ec993
Supriyo Saha <mm.saha1982@gmail.com>
2023-10-31 03:33:30 +0530

    small bug fix

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -15,6 +15,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
 import Loader from "@/components/shared/Loader";
+import { Link } from "react-router-dom";
 
 const SignupForm = () => {
 
@@ -114,6 +115,16 @@ const SignupForm = () => {
               </div>
             ) : "Sign up"}
           </Button>
+
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1">
+              Log in
+            </Link>
+          </p>
+
         </form>
       </div>
     </Form>
