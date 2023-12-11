c96aebac76cef8e159bdeaaf7a887e4550a53398
Supriyo Saha <mm.saha1982@gmail.com>
2023-12-11 22:22:04 +0530

    fix

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -5,7 +5,9 @@ import {
   useInfiniteQuery,
 } from "@tanstack/react-query";
 import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
-import { INewUser } from "@/types";
+import { INewPost, INewUser } from "@/types";
+import { CreatePost } from "@/_root/pages";
+import { QUERY_KEYS } from "./queryKeys";
 
 //Auth queries
 export const useCreateUserAccountMutation = () => {
@@ -25,3 +27,15 @@ export const useSignOutAccount = () => {
         mutationFn:  signOutAccount
     })
 }
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (post: INewPost) => CreatePost(post),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+      },
+    });
+  };
